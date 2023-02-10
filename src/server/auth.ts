c1903fbc8e9128954edb9b450b1e4750017503b9
rcrosbourne <rcrosbourne@gmail.com>
2023-02-10 02:06:22 -0500

    Fixed session issue

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,13 +1,6 @@
 import type { GetServerSidePropsContext } from "next";
-import {
-  getServerSession,
-  type NextAuthOptions,
-  type DefaultSession,
-} from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "../env.mjs";
-import { prisma } from "./db";
+import { getServerSession, type DefaultSession } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
 
 /**
  * Module augmentation for `next-auth` types.
@@ -30,40 +23,6 @@ declare module "next-auth" {
   //   // role: UserRole;
   // }
 }
-
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks,
- * etc.
- *
- * @see https://next-auth.js.org/configuration/options
- **/
-export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
-      }
-      return session;
-    },
-  },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     **/
-  ],
-};
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the
